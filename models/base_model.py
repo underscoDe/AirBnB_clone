6a7f689eba3d9e136dd6ed077f9b642dd7a35724
Arsene Awounou <developer.awounou@gmail.com>
2022-01-29 12:59:26 +0100

    create BaseModel from a dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,25 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the BaseModel class.
 
         Args:
             self (BaseModel): the current instance
+            args (any): not used here
+            kwargs (dict): dictionary of key/value pairs attributes
         """
         # public instance attributes
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs):
+            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(value, iso_format)
+                else:
+                    self.__dict__[key] = value
 
     # public instance methods
     def save(self):
