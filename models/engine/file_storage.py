9d7dfa6877aa00caaed33c949d44d4fb4a097fb8
Arsene Awounou <developer.awounou@gmail.com>
2022-01-29 16:14:36 +0100

    fix PEP8 coding style issue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,9 @@ class FileStorage:
     def save(self):
         """Serializes __objects to the JSON file"""
         objects_copy = FileStorage.__objects
-        objects_dict = {obj: objects_copy[obj].to_dict() for obj in objects_copy.keys()}
+        objects_dict = {
+            obj: objects_copy[obj].to_dict() for obj in objects_copy.keys()
+        }
 
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objects_dict, f)
