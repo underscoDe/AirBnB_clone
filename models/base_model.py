ccf562e8882b925e09654f27e58dba43ed11f701
Arsene Awounou <developer.awounou@gmail.com>
2022-01-29 12:03:49 +0100

    override __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -21,15 +21,19 @@ class BaseModel:
     # public instance methods
     def save(self):
         """Updates the public instance attribute updated_at \
-            with the current datetime"""
+            with the current datetime."""
         self.updated_at = datetime.today()
 
     def to_dict(self):
         """Returns a dictionary containing all \
-            keys/values of __dict__ of the instance"""
+            keys/values of __dict__ of the instance."""
         dict_copy = __dict__.copy()
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         dict_copy["__class__"] = self.__class__.__name__
 
         return (dict_copy)
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
